23619c244766557ae9bbc71908860545486e6437
Ifediora E C <ifedioraelochukwuc@gmail.com>
2024-06-26 10:24:38 +0200

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
 set -e
-
 sudo apt-get install -y python3 python3-pip tar
 pip3 install numpy
-
 export PATH=$PWD/riscv/bin:$PATH
 mkdir -p riscv
 wget https://github.com/stnolting/riscv-gcc-prebuilt/releases/download/rv32i-131023/riscv32-unknown-elf.gcc-13.2.0.tar.gz
