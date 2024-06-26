8138ef0f8bf6cb2e13f2eeb4ab43a0b3726bddff
Ifediora E C <ifedioraelochukwuc@gmail.com>
2024-06-26 06:15:42 +0200

    add workflows

---
+++ .github/scripts/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+sudo apt-get install -y python3 python3-pip tar
+pip3 install numpy
+
+export PATH=$PWD/riscv/bin:$PATH
+mkdir -p riscv
+wget https://github.com/stnolting/riscv-gcc-prebuilt/releases/download/rv32i-131023/riscv32-unknown-elf.gcc-13.2.0.tar.gz
+tar -xzvf riscv32-unknown-elf.gcc-13.2.0.tar.gz riscv
+
+cd $GITHUB_WORKSPACE/SW/compiler
+make clean all
+cd $GITHUB_WORKSPACE/SW/fs
+python3 bin2c.py
+cd ..
+make clean all -f makefile.kernels RISCV_PATH=$GITHUB_WORKSPACE/riscv RISCV_NAME=riscv32-unknown-elf
+make clean all
