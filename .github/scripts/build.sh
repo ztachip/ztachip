ef9967cdb17c0d3d937a25acf3a90a1ca4495bc4
Ifediora E C <ifedioraelochukwuc@gmail.com>
2024-06-26 10:51:13 +0200

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
 set -e
+
 sudo apt-get install -y python3 python3-pip tar
 pip3 install numpy
+
+export BUILD_DIR=$PWD
 export PATH=$PWD/riscv/bin:$PATH
 mkdir -p riscv && cd riscv
 wget https://github.com/stnolting/riscv-gcc-prebuilt/releases/download/rv32i-131023/riscv32-unknown-elf.gcc-13.2.0.tar.gz
@@ -13,4 +17,4 @@ cd ../fs
 python3 bin2c.py
 cd ..
 make clean all -f makefile.kernels
-make clean all RISCV_PATH=../riscv/bin RISCV_NAME=riscv32-unknown-elf
+make clean all RISCV_PATH=$BUILD_DIR/riscv/ RISCV_NAME=riscv32-unknown-elf
